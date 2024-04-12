4ff21a7e39f32f090e48e20a8a4c1dd1561c874d
Pavel Kral <pavel@pavelkral.eu>
2024-04-12 11:10:26 +0200

    Implement measuring variable prompt size

--- latencylab/main.py
+++ latencylab/main.py
@@ -1,6 +1,7 @@
 import typer
 
-from latencylab import run
+from latencylab import run, prompt
 
 app = typer.Typer()
-app.add_typer(run.app, name="run", help="Run LLM benchmark")
+app.add_typer(run.app, name="latency", help="Run LLM benchmark on constant prompt")
+app.add_typer(prompt.app, name="prompt", help="Run LLM benchmark on various prompt sizes")
